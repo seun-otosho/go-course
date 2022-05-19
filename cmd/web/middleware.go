8760458c67255770a1e66e3c9fa2e6fc24d578f9
Oluw@$£un <toloruntotosho@gmail.com>
2022-05-19 04:13:40 +0100

    Middlewares before implementing sessions

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/justinas/nosurf"
+	"log"
+	"net/http"
+)
+
+func Write2Console(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hit the page")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csrfHandler
+}
